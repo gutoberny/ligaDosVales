3de1d71eb581275385fa9b9874bd6cc4ca8a83e0
Gustavo Berny <pelotas.berny93@gmail.com>
2025-08-27 22:57:52 -0300

    ajustado logo e layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -17,11 +17,36 @@ const Layout = () => {
         position="fixed"
         sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
+        {/* A Toolbar precisa de position: 'relative' para que o posicionamento absoluto do filho funcione */}
+        <Toolbar sx={{ position: "relative" }}>
+          <RouterLink to="/">
+            <img
+              src="/img/logo_wilsons.png"
+              alt="Logo da Liga dos Vales de Voleibol"
+              style={{ height: 75, marginRight: 10, display: "flex" }}
+            />
+          </RouterLink>
+
+          {/* --- CORREÇÃO APLICADA AQUI --- */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              // 1. Posiciona o título de forma absoluta em relação à Toolbar
+              position: "absolute",
+              // 2. Move o ponto de início para o centro exato (horizontal e vertical)
+              left: "50%",
+              top: "50%",
+              // 3. Usa o 'transform' para corrigir o alinhamento, puxando o elemento
+              // de volta pela metade da sua própria largura e altura.
+              transform: "translate(-50%, -50%)",
+            }}
+          >
             Liga dos Vales de Voleibol
           </Typography>
 
+          {/* Este espaçador continua a empurrar o botão de login para a direita */}
           <Box sx={{ flexGrow: 1 }} />
 
           {!user && (
@@ -39,7 +64,7 @@ const Layout = () => {
                 },
               }}
             >
-              dmin
+              Login Admin
             </Button>
           )}
         </Toolbar>
